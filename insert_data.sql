8a9c06984698fa08887ebdb3c19b3f7ad4a34d71
Atharva Gaikwad <atharva.22311542@viit.ac.in>
2025-12-23 10:52:56 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,26 @@
+-- Insert sample data
+INSERT INTO departments VALUES
+(1,'IT','Pune'),
+(2,'HR','Mumbai'),
+(3,'Finance','Delhi');
+
+INSERT INTO employees VALUES
+(101,'Amit',1,'2022-01-10'),
+(102,'Neha',1,'2021-06-15'),
+(103,'Rahul',2,'2023-03-01'),
+(104,'Priya',3,'2020-09-20'),
+(105,'Karan',1,'2023-07-11');
+
+INSERT INTO salaries VALUES
+(101,60000),
+(102,75000),
+(103,50000),
+(104,90000),
+(105,45000);
+
+INSERT INTO attendance VALUES
+(101,'2024-01-01','Present'),
+(101,'2024-01-02','Absent'),
+(102,'2024-01-01','Present'),
+(103,'2024-01-01','Present'),
+(104,'2024-01-02','Absent');
